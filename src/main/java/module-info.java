c3274c098989280f0fed567e6da115075a116da9
wiggapony0925 <ninjeff06@gmail.com>
2023-08-01 17:29:23 -0400

    added dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module com.example.code_changer {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-            requires com.dlsc.formsfx;
-                requires org.kordamp.ikonli.javafx;
-                
+    requires javafx.base;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.fxmisc.richtext;
+
     opens com.example.code_changer to javafx.fxml;
     exports com.example.code_changer;
 }
