d557146e3ae195a79c5f04aa60d368c8927b6aad
wiggapony0925 <ninjeff06@gmail.com>
2023-08-02 14:36:18 -0400

    varibles and module for client

--- src/main/java/com/example/code_changer/OpenAiClient.java
+++ src/main/java/com/example/code_changer/OpenAiClient.java
@@ -1,4 +1,21 @@
 package com.example.code_changer;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 public class OpenAiClient {
+    private static final String API_BASE_URl = "";
+    private static final String APi_ENDPOINT_TRANSLATE = "";
+
+    private String apiKey;
+
+    public OpenAiClient(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+
 }
