7d947fecb5d9cf61ebf0ce0aebfd88e45232d7be
wiggapony0925 <ninjeff06@gmail.com>
2023-08-02 16:34:42 -0400

    final mvp

--- src/main/java/com/example/code_changer/HelloController.java
+++ src/main/java/com/example/code_changer/HelloController.java
@@ -2,10 +2,8 @@ package com.example.code_changer;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
@@ -13,30 +11,61 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
-import java.util.Objects;
-
-
 public class HelloController implements Initializable {
-
 
     @FXML
     private Label OpenaiText;
     @FXML
     private ComboBox<String> inputLanguageComboBox;
-
     @FXML
     private ComboBox<String> outputLanguageComboBox;
-
     @FXML
     private TextField apiKeyField;
-
     @FXML
     private TextArea outputTextArea; // Add this declaration
-
     @FXML
     private TextArea inputTextArea;
+    @FXML
+    private Button downloadButton;
+
+    @FXML
+    private void downloadOutputFile() {
+        FileChooser fileChooser = new FileChooser();
+
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text Files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        File file = fileChooser.showSaveDialog(null);
+
+        if (file != null) {
+            try {
+                // Get the content from the outputTextArea
+                String translatedCode = outputTextArea.getText();
+
+                // Write the content to the selected file
+                Files.write(file.toPath(), translatedCode.getBytes());
+
+                // Show a success message to the user
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Download Complete");
+                alert.setHeaderText(null);
+                alert.setContentText("The translated code has been downloaded successfully.");
+                alert.showAndWait();
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                // Show an error message to the user if something goes wrong
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("An error occurred while downloading the translated code.");
+                alert.showAndWait();
+            }
+        }
+    }
 
     @FXML
     private void importFile() {
@@ -68,10 +97,9 @@ public class HelloController implements Initializable {
         }
     }
 
-
     @FXML
     private void translateCode() {
-        String inputCode = ""; // Replace this with your desired input text
+        String inputCode = inputTextArea.getText(); // Get the input code from the inputTextArea
         String inputLanguage = inputLanguageComboBox.getValue();
         String outputLanguage = outputLanguageComboBox.getValue();
         String apiKey = apiKeyField.getText(); // Access API key from the TextField
@@ -85,6 +113,7 @@ public class HelloController implements Initializable {
         OpenAiClient openAiClient = new OpenAiClient(apiKey);
         String translatedCode = openAiClient.translateCode(inputCode, inputLanguage, outputLanguage);
 
+        // Set the translated code in the outputTextArea
         outputTextArea.setText(Objects.requireNonNullElse(translatedCode, "Translation failed. Please try again"));
     }
 
@@ -96,5 +125,4 @@ public class HelloController implements Initializable {
         inputLanguageComboBox.setValue("None");
         outputLanguageComboBox.setValue("Choose");
     }
-
 }
